42489ccf9521366da03a30350e369684afc87e5b
Sanchit joshi <132713080+Sanchit-Joshi@users.noreply.github.com>
2024-07-16 20:23:03 +0530

    still creating the frontend of it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component, useState } from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Input from "./components/Input";
+import Hero from "./components/Hero";
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>  )
-}
+      
+      <div className="pt-5 lg:pt-[5.25rem] overflow-hidden">
+       <Header />
+       <Hero />
+       {/* <Input /> */}
+      </div>
+      
+      
+      <ButtonGradient/>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
